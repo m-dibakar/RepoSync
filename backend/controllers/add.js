10df8bb6bec43b4e644d058176f2a0dbf6318e28
m-dibakar <malakar.dibakar@icloud.com>
2026-02-21 01:43:03 +0530

    created add command

--- backend/controllers/add.js
+++ backend/controllers/add.js
@@ -1,5 +1,18 @@
-async function addRepo() {
-  console.log("Add command called");
+const fs = require("fs").promises;
+const path = require("path");
+
+async function addRepo(filePath) {
+  const repoPath = path.resolve(process.cwd(), ".repoGit");
+  const stagingPath = path.join(repoPath, "stage");
+
+  try {
+    await fs.mkdir(stagingPath, { recursive: true });
+    const fileName = path.basename(filePath);
+    await fs.copyFile(filePath, path.join(stagingPath, fileName));
+    console.log(`${fileName} added to the staging area`);
+  } catch (error) {
+    console.error("Error adding file: ", error);
+  }
 }
 
 module.exports = { addRepo };
