933615401b766ec3a1f7d2b66058355ca761a302
m-dibakar <malakar.dibakar@icloud.com>
2026-02-21 04:12:32 +0530

    Created push command and pushed it to s3

--- backend/config/aws-config.js
+++ backend/config/aws-config.js
@@ -1,8 +1,9 @@
+require("dotenv").config();
 const AWS = require("aws-sdk");
 
 AWS.config.update({ region: "ap-south-1" });
 
-const s3 = AWS.S3();
+const s3 = new AWS.S3();
 
 const S3_BUCKET = "reposync.bucket";
 
