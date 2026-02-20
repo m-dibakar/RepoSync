1ed1d0b944a34f7cbfa0774b5a9f784bab3d082c
m-dibakar <malakar.dibakar@icloud.com>
2026-02-21 01:05:21 +0530

    init command created

--- backend/controllers/init.js
+++ backend/controllers/init.js
@@ -1,5 +1,21 @@
+const fs = require("fs").promises;
+const path = require("path");
+
 async function initRepo() {
-  console.log("Init command called");
+  const repoPath = path.resolve(process.cwd(), ".repoGit");
+  const commitsPath = path.join(repoPath, "commits");
+
+  try {
+    await fs.mkdir(repoPath, { recursive: true });
+    await fs.mkdir(commitsPath, { recursive: true });
+    await fs.writeFile(
+      path.join(repoPath, "config.json"),
+      JSON.stringify({ bucket: "s3 bucket" }),
+    );
+    console.log("Repository initialised!");
+  } catch (error) {
+    console.error("Error initialising repository", error);
+  }
 }
 
 module.exports = { initRepo };
