b45f81358a7bad95e40ee8d543cdd04b8ed85c67
m-dibakar <malakar.dibakar@icloud.com>
2026-02-22 01:06:03 +0530

    Set Issue functionality

---
+++ backend/controllers/issueController.js
@@ -0,0 +1,27 @@
+const createIssue = (req, res) => {
+  res.send("Issue created");
+};
+
+const updateIssueById = (req, res) => {
+  res.send("Issue created");
+};
+
+const deleteIssueById = (req, res) => {
+  res.send("Issue created");
+};
+
+const getAllIssues = (req, res) => {
+  res.send("Issue created");
+};
+
+const getIssueById = (req, res) => {
+  res.send("Issue created");
+};
+
+module.exports = {
+  createIssue,
+  updateIssueById,
+  deleteIssueById,
+  getAllIssues,
+  getIssueById,
+};
